f83ddd7038114fe4e5fdd1f675d0916dc92bc673
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-12-07 22:49:02 +0300

    Added comments to the module

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
+"""0-add module"""
+
+
 def add(a: float, b: float) -> float:
+    """Type-annonated python function that takes
+    a floats a and b and returns the sum as a float.
+    """
     return a + b
