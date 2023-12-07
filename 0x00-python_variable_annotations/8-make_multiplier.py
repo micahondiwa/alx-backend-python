ceac506e72f44df68a96f5ae4cca391bd941e3cd
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-12-07 23:23:03 +0300

    Make multiplier

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""8-make_multiplier module"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Type-annotated function that takes a float multiplier as argument and
+    returns a function that multiplies a float by multiplier
+    """
+    return lambda x: x * multiplier
