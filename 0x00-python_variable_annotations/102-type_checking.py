b1f82b433b954a2c3108ee93535d6554d8bc26e8
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-12-07 23:30:01 +0300

    Type checking

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""102-type_checking module"""
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    Creates multiple copies of items in a tuple.
+    """
+    zoomed_in: List = [item for item in lst for i in range(int(factor))]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
