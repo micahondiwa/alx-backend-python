aedb2bc91a9edc7dffd03c4acc134a4e059e413d
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-12-07 23:20:14 +0300

    to kv

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    A function that takes a string k and an int OR float v
+    as arguments and returns a tuple [k: str, square(v): float]
+    """
+    return (k, float(v**2))
