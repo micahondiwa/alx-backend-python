176f04842c1e1573c34f6481808824bca3de4106
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-12-07 22:52:16 +0300

    Concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""1-concat modiule"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    A type-annonated function that takes strings str1 and str2
+    and returns a concatenated tsring.
+    """
+    return str1 + str2
