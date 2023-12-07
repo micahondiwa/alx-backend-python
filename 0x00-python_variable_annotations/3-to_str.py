3d4cedaebe9dc50dcf12ff035900c9ca7b34d47f
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-12-07 23:03:55 +0300

    Str representation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""3-to_str module"""
+
+
+def to_str(n: float) -> str:
+    """
+    A type-annotated function that takes n and returns the
+    string representation of the float.
+    """
+    return str(n)
