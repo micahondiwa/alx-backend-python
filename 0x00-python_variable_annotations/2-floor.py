b37f6c63554d3fbc162eb22e1d0f3403e4d72ad9
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-12-07 22:58:43 +0300

    Floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""2-floor module"""
+
+import math
+
+
+def floor(n: float) -> float:
+    """
+    A type-annotated function which takes n and returns
+    the floor of the float.
+    """
+    return math.floor(n)
