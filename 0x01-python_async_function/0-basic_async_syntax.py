77b4a5e18d25af78ac323ddb91706d87295b3754
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-12-11 21:05:49 +0300

    Wait time

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,3 +3,15 @@
 
 
 import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Takes an int argument that waits for a random
+    delay between o and max_delay and eventyally
+    returns it.
+    """
+    waiting_time = random.random() * max_delay
+    await asyncio.sleep(waiting_time)
+    return waiting_time
