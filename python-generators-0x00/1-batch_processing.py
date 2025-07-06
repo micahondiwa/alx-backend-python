770a0eef14cf9f66615d4c5c63cab94da8bd856f
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-07-06 10:32:29 +0300

    Batch processing large data

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -3,4 +3,9 @@
 import mysql.connector
 
 def stream_user_in_batches(batch_size):
-    co
+    connection = mysql.connector.connect(
+        host='127.0.0.1',
+        user='root',
+        password='Admin@123',
+        database='ALX_prodev'
+    )
