f59cc97d20d0a9a8ebfc18e5bda23d18daed9c60
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-07-06 10:45:05 +0300

    Lazy loading paginated data

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -5,4 +5,5 @@ import seed
 def paginate_users(page_size, offset):
     connection = seed.connect_to_prodev()
     cursor = connection.cursor(dictionary=True)
+    cursor.execute(f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
     
