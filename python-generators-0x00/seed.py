66539e45b376dbe21373f332a0c1062700fdb852
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-07-06 08:19:31 +0300

    Mysql connector

--- python-generators-0x00/seed.py
+++ python-generators-0x00/seed.py
@@ -15,3 +15,23 @@ def connect_db():
     except mysql.connector.Error as err:
         print(f"Error conncting: {err}")
         return None
+
+def create_database(connection):
+    try:
+        cursor=connection.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS ALX_prodev;")
+        cursor.close()
+    except mysql.connector.Error as err:
+        print(f"Failed creating database: {err}")
+
+def connect_to_prodev():
+    try:
+        return mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password=' ',
+            database='ALX_prodev'
+        )
+    except mysql.connector.Error as err:
+        print(f"Error connecting to ALX_prodev: {err}")
+        return None
