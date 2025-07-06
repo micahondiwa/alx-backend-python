2d6551137c6f47f78e7a7ba12ad92aa095c81392
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-07-06 08:13:14 +0300

    Mysql connector

--- python-generators-0x00/seed.py
+++ python-generators-0x00/seed.py
@@ -4,3 +4,13 @@ import mysql.connector
 import csv
 import uuid
 from mysql.connector import errorcode 
+
+def connect_db():
+    try:
+        return mysql.connector.connect(
+            host="localhost",
+            user="rot",
+            password=" "
+        )
+    except mysql.connector.Error as err:
+        
