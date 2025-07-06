85d3c404498118dbcdb3986da7e34703f2b6641d
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-07-06 10:25:25 +0300

    Stream users

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
 
 import mysql.connector
+
+def stream_users():
+    connection = mysql.connector.connect(
+        host='127.0.0.1',
+        user='root',
+        password='Admin@123',
+        database='ALX_prodev'
+    )
+
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+
+    for row in cursor:
+        yield row 
+    
+    cursor.close
+    connection.close()
