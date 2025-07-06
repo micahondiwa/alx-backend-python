f56c52123c5fc12c2ff3d2556fae09a800694ccb
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-07-06 10:36:27 +0300

    Batch processing large data

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -21,3 +21,9 @@ def stream_user_in_batches(batch_size):
 
     cursor.close()
     connection.close()
+
+def batch_processing(batch_size):
+    for batch in stream_user_in_batches(batch_size):
+        for user in batch:
+            if user['age'] > 25:
+                print(user)
