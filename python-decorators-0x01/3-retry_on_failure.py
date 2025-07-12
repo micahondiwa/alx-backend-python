8a2ff33d1fff31fc1a8ba14377ffbcd853cc568b
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-07-12 11:32:33 +0300

    Using decorators to retry database queries

--- python-decorators-0x01/3-retry_on_failure.py
+++ python-decorators-0x01/3-retry_on_failure.py
@@ -1,3 +1,9 @@
 import time
 import sqlite3
 import functools
+
+@with_db_connection
+@retry_on_failure(retry=3, delay=1)
+def fetch_users_with_retry(conn):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
