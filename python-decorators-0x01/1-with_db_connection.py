8bd42828f3165d3399ead6f733102c5fc915fefe
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-07-12 11:17:26 +0300

    Handle a database connection with a decorator

--- python-decorators-0x01/1-with_db_connection.py
+++ python-decorators-0x01/1-with_db_connection.py
@@ -3,6 +3,8 @@ import functools
 
 def with_db_connection(func):
     @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('users.db')
     
 
 @with_db_connection
