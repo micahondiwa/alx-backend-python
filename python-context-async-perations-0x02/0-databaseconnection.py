6cdd507c481d073871b06203cec97bc17c1b50b0
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-07-12 15:46:56 +0300

    Concurrent Asynchronous database queries

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+class DatabaseConnection:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = None
+    
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        return self.conn
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.conn:
+            self.conn.close()
+        # Return False to propagate any exceptions
+        return False
+
+# Using the context manager to query users
+with DatabaseConnection('users.db') as conn:
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    results = cursor.fetchall()
+    
+    # Print the results
+    print("Users in database:")
+    for row in results:
+        print(row)
