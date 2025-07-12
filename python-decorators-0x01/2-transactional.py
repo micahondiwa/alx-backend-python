43aba096eb11a019cb41fcd54760ca9fc3b4697f
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-07-12 11:22:25 +0300

    Transaction management decorator

--- python-decorators-0x01/2-transactional.py
+++ python-decorators-0x01/2-transactional.py
@@ -4,4 +4,5 @@ import functools
 @with_db_connection
 @transactional
 def update_user_email(conn, user_id, new_email):
-    
+    cursor = conn.cursor()
+    cursor.execute("UPDATE users SET email = ? WHERE id = ? ", (new_email, user_id))
