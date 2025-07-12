15d6ed4bfd0b5d3c827c60316657f3d0f6254b05
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-07-12 11:25:42 +0300

    Transaction management decorator

--- python-decorators-0x01/2-transactional.py
+++ python-decorators-0x01/2-transactional.py
@@ -3,7 +3,10 @@ import functools
 
 def with_db_connection(func):
     @functools.wraps(func)
-    
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('users.db')
+        try:
+            result = func(conn, *args, **kwargs)
 
 
 @with_db_connection
