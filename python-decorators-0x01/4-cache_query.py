e636d046477be809dff8865502be4dfd686419b4
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-07-12 11:43:21 +0300

    Using decorators to cache Database Queries

--- python-decorators-0x01/4-cache_query.py
+++ python-decorators-0x01/4-cache_query.py
@@ -10,3 +10,6 @@ def fecth_users_with_cache(conn, query):
     cursor = conn.cursor()
     cursor.execute(query)
     return cursor.fetchall()
+
+#### First call will cache the result
+users = fecth_users_with_cache(query="SELECT * FROM users")
