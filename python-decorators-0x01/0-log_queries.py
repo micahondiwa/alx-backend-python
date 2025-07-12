b749d4995cf9b3c1db2849fbb6a606c3ccbee228
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-07-12 10:42:56 +0300

    Timestamp fixed

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -6,9 +6,12 @@ from datetime import datetime
 def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **Kwargs):
-        query = Kwargs.get('query')
-        if not query and len(args) > 0:
-            query = args[0]
+        query = Kwargs.get('query', None)
+        if query is None and len(args) > 0:
+            query = args[0] if isinstance(args[0], str) else None
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"[{timestamp}] Executing SQL quey: {query}")
+
         return func(*args, **Kwargs)
     return wrapper
 
