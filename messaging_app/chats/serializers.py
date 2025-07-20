cdf385091e0274c1bea23fa54b625b4dd2f193f2
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-07-20 21:24:17 +0300

    Serializers

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,10 +1,7 @@
 from rest_framework import serializers
 from .models import User, Conversation, Message
 
-
-# ----------------------------
 # User Serializer
-# ----------------------------
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         write_only=True,
