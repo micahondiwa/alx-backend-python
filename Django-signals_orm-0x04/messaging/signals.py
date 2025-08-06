ca7905cb2bf40d6bbdc933decdaa25d4802f39fd
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-06 13:43:05 +0300

    signals

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,7 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
+
 
 @receiver(post_save, sender=Message)
 def create_notification_on_message(sender, instance, created, **kwargs):
@@ -9,3 +10,20 @@ def create_notification_on_message(sender, instance, created, **kwargs):
             user=instance.receiver,
             message=instance
         )
+
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if instance.pk:  # only if message already exists
+        try:
+            original = Message.objects.get(pk=instance.pk)
+            if original.content != instance.content:
+                # Log old content
+                MessageHistory.objects.create(
+                    message=original,
+                    old_content=original.content
+                )
+                # Mark as edited
+                instance.edited = True
+        except Message.DoesNotExist:
+            pass
