723805bd5daa6d1ba5d76d8972d68fa3ba8e35b9
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-06 13:39:23 +0300

    App

---
+++ Django-signals_orm-0x04/messaging/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+class MessagingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'messaging'
+
+    def ready(self):
+        import messaging.signals  # ✅ Load signals when app is ready
