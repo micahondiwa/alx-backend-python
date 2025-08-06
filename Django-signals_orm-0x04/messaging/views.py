3336312b2b827ba89a2d6b588cb14fef73675d0d
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-06 13:52:09 +0300

    Delete

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,21 +1,17 @@
-from django.db.models.signals import post_delete
-from django.dispatch import receiver
-from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.http import HttpResponseNotAllowed
 
-from .models import Message, Notification, MessageHistory
+# Keep existing imports and views above this
 
-@receiver(post_delete, sender=User)
-def cleanup_user_related_data(sender, instance, **kwargs):
-    # Delete all messages sent or received by the user
-    Message.objects.filter(sender=instance).delete()
-    Message.objects.filter(receiver=instance).delete()
-
-    # Delete all notifications for the user
-    Notification.objects.filter(user=instance).delete()
-
-    # Delete all message histories they edited
-    MessageHistory.objects.filter(edited_by=instance).delete()
-
-    # Optionally: delete histories of messages sent by user
-    user_messages = Message.objects.filter(sender=instance)
-    MessageHistory.objects.filter(message__in=user_messages).delete()
+@login_required
+def delete_user(request):
+    """
+    View to allow the logged-in user to delete their own account.
+    Triggers cleanup via post_delete signal.
+    """
+    if request.method == "POST":
+        user = request.user
+        user.delete()
+        return redirect("home")  # Adjust to your actual homepage or login redirect
+    return HttpResponseNotAllowed(["POST"])
