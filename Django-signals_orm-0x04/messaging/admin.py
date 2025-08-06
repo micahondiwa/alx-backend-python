68c5d9ce73dc208a58e69ddc5eb6800977a089e9
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-06 13:43:56 +0300

    Admin

--- Django-signals_orm-0x04/messaging/admin.py
+++ Django-signals_orm-0x04/messaging/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
 
 admin.site.register(Message)
 admin.site.register(Notification)
+admin.site.register(MessageHistory)
