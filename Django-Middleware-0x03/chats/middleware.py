dae98e24d1e7219864c3ff50adaf9ca944bd49f8
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-08-06 11:49:32 +0300

    Middleware

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,1 +1,23 @@
-# Middleware
+# listings/middleware.py
+import logging
+from datetime import datetime
+
+# Configure a logger
+logger = logging.getLogger(__name__)
+handler = logging.FileHandler('request_logs.log')  # Logs will be written here
+formatter = logging.Formatter('%(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+class RequestLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        user = request.user if request.user.is_authenticated else 'Anonymous'
+        log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
+        logger.info(log_message)
+
+        response = self.get_response(request)
+        return response
